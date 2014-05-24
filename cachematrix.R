9255cb780353819d5b07d4362d215c7f41f5f407
jdbcoursera <browderj@gmail.com>
2014-05-24 16:07:36 +0300

    Added cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to define a matrix object with a cached inverse
 
-## Write a short comment describing this function
+## Defines a matrix object whose inverse, once computed
+## will be cached in the object.
+## has methods to get (returns the matrix)
+## set (set the matrix)
+## getInverse (returns the inverse matrix)
+## and setInverse (set the inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(matrix){
+        x <<- matrix
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(m) inverse <<-m
+    getInverse <- function() inverse
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## if the matrix inverse is already cached, return it
+## otherwise, calculate it, cache it, and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)){
+        return(inverse)
+    }
+    matrix = x$get()
+    inverse <- solve(matrix)
+    x$setInverse(inverse)
+    inverse
 }
